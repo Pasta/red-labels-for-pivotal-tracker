a63d0aa5b3588d3ba91ca7051d53ad10a875046b
Vincent Coste <coste.vincent@gmail.com>
2016-01-26 16:55:56 -0800

    better label matcher

--- src/inject/isLabelEligible.js
+++ src/inject/isLabelEligible.js
@@ -1,12 +1,15 @@
 var isLabelEligible = function isLabelEligible(labelText) {
-  if (labelText === 'high cost, ') {
-    return 'high_cost';
+  if (!!labelText.match(/\b(?:high cost)\b/)) {
+      return 'high_cost';
   }
-  if (labelText === 'medium cost, ') {
-    return 'medium_cost';
+  if (!!labelText.match(/\b(?:medium cost)\b/)) {
+      return 'medium_cost';
   }
-  if (labelText === 'low cost, ') {
-    return 'low_cost';
+  if (!!labelText.match(/\b(?:low cost)\b/)) {
+      return 'low_cost';
+  }
+  if (!!labelText.match(/\b(?:build)\b/)) {
+      return 'build';
   }
   if (!!labelText.match(/\b(?:blocked|needs)\b/)) {
       return 'blocked';
